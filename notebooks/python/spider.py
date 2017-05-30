c1817fd2a24d6fe5d27ee40f33584a8c0b844a1d
Zhiping Wu <zpwu@thoughtworks.com>
2017-05-30 21:22:30 +0800

    add spider

---
+++ notebooks/python/spider.py
@@ -0,0 +1,42 @@
+#!/usr/bin env python3
+
+from Crypto.Cipher import AES
+import base64
+import requests
+import json
+import codecs
+import time
+
+
+header = {
+    'Host':"music.163.com",
+    'Accept-Language':"zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3",
+    'Accept-Encoding':"gzip, deflate",
+    'Content-Type':"application/x-www-form-urlencoded",
+    'Cookie':"_ntes_nnid=754361b04b121e078dee797cdb30e0fd,1486026808627; _ntes_nuid=754361b04b121e078dee797cdb30e0fd; JSESSIONID-WYYY=yfqt9ofhY%5CIYNkXW71TqY5OtSZyjE%2FoswGgtl4dMv3Oa7%5CQ50T%2FVaee%2FMSsCifHE0TGtRMYhSPpr20i%5CRO%2BO%2B9pbbJnrUvGzkibhNqw3Tlgn%5Coil%2FrW7zFZZWSA3K9gD77MPSVH6fnv5hIT8ms70MNB3CxK5r3ecj3tFMlWFbFOZmGw%5C%3A1490677541180; _iuqxldmzr_=32; vjuids=c8ca7976.15a029d006a.0.51373751e63af8; vjlast=1486102528.1490172479.21; __gads=ID=a9eed5e3cae4d252:T=1486102537:S=ALNI_Mb5XX2vlkjsiU5cIy91-ToUDoFxIw; vinfo_n_f_l_n3=411a2def7f75a62e.1.1.1486349441669.1486349607905.1490173828142; P_INFO=m15527594439@163.com|1489375076|1|study|00&99|null&null&null#hub&420100#10#0#0|155439&1|study_client|15527594439@163.com; NTES_CMT_USER_INFO=84794134%7Cm155****4439%7Chttps%3A%2F%2Fsimg.ws.126.net%2Fe%2Fimg5.cache.netease.com%2Ftie%2Fimages%2Fyun%2Fphoto_default_62.png.39x39.100.jpg%7Cfalse%7CbTE1NTI3NTk0NDM5QDE2My5jb20%3D; usertrack=c+5+hljHgU0T1FDmA66MAg==; Province=027; City=027; _ga=GA1.2.1549851014.1489469781; __utma=94650624.1549851014.1489469781.1490664577.1490672820.8; __utmc=94650624; __utmz=94650624.1490661822.6.2.utmcsr=baidu|utmccn=(organic)|utmcmd=organic; playerid=81568911; __utmb=94650624.23.10.1490672820",
+    'Connection':"keep-alive",
+    'Referer':'http://music.163.com/'
+}
+
+proxies = {
+    'http:':'http://121.232.146.184',
+    'https:':'https://144.255.48.197'
+}
+
+second_param = '{rid:"", offset: "0", total:"true", limit:"20", csrf_token:""}'
+third_param = "00e0b509f6259df8642dbc35662901477df22677ec152b5ff68ace615bb7b725152b3ab17a876aea8a5aa76d2e417629ec4ee341f56135fccf695280104e0312ecbda92557c93870114af6c9d05c4f7f0c3685b7a46bee255932575cce10b424d813cfe4875d3e82047b97ddef52741d546b8e289dc6935b3ece0462db0a22b8e7"
+forth_param = "0CoJUm6Qyw8W8jud"
+
+def get_param(page):
+    iv = "0102030405060708"
+    first_key = forth_param
+    second_key = 16 * 'F'
+    if (page == 1):
+        first_param = '{rid:"", offset: "0", total:"true", limit:"20", csrf_token:""}'
+        h_encText = AES_encrypt(first_param, first_key, iv)
+    else:
+        offset = str((page -1 ) * 20)
+        first_param = '{rid:"", offset: "%s", total:"%s", limit:"20", csrf_token:""}' % (offset, 'false')
+        h_encText = AES_encrypt(first_param, second_key, iv)
+    h_encText = AES_encrypt(h_encText, second_key, iv)
+    return h_encText
