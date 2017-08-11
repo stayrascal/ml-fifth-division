8036e54d451ab692c3e53264f8ca9478faa8fa2e
Zhiping Wu <zpwu@thoughtworks.com>
2017-08-11 23:41:39 +0800

    update frcnn count

--- notebooks/keras-frcnn/test_frcnn_count.py
+++ notebooks/keras-frcnn/test_frcnn_count.py
@@ -72,4 +72,83 @@ def save_to_video():
     videowriter.release()
     cv2.destroyAllWindows()
 
+def format_img(img, C):
+    img_min_side = float(C.im.size)
+    (height, width, _) = img.shape
+
+    if width <= height:
+        f = img_min_side/width
+        new_height = int(f * height)
+        new_width = int(img_min_side)
+    else:
+        f = img_min_side/height
+        new_width = int(f * width)
+        new_height = int(img_min_side)
+    img = cv2.resize(img, (new_width, new_height), interpolation=cv2.INTER_CUBIC)
+    img = img[:, :, (2, 1, 0)]
+    img = img.astype(np.float32)
+    img[:, :, 0] -= C.img_channels_mean[0]
+    img[:, :, 1] -= C.img_channels_mean[1]
+    img[:, :, 2] -= C.img_channels_mean[2]
+    img /= C.img_scaling_factor
+    img = np.transpose(img, (2, 0, 1))
+    img = np.expand_dims(img, axis=0)
+    return img
+
+def accumulate(l):
+    it = itertools.groupby(l, operator.itemgetter(0))
+    for key, subiter in it:
+        yield key, sum(item[1] for item in subiter)
+
+def main():
+    cleanup()
+    sys.setrecursionlimit(40000)
+    config_output_filename = 'config.pickle'
+
+    with open(config_output_filename, 'r') as f_in:
+        C = pickle.load(f_in)
+
+    # turn off any data augmentation at test time
+    C.use_horizontal_flips = False
+    C.use_vertical_flips = False
+    C.rot_90 = False
+    class_mapping = C.class_mapping
+
+    if 'bg' not in cladd_mapping:
+        class_mapping['bg'] = len(class_mapping)
+
+    class_mapping = {v : k for k, v in class_mapping.iteritems()}
+    print(class_mapping)
+    class_to_color = {class_mapping[v]: np.random.randint(0, 255, 3) for v in class_mapping}
+    C.num_rois = num_rois
+
+    if K.image_dim_ordering() == 'th':
+        input_shape_img = (3, None, None)
+        input_shape_features = (1024, None, None)
+    else:
+        input_shape_img = (None, None, 3)
+        input_shape_features = (None, None, 1024)
+
+    img_input = Input(shape=input_shape_img)
+    roi_input = Input(shape=(C.num_rois, 4))
+    feature_map_input = Input(shape=input_shape_features)
+
+    # define the base network (resnet here, can be VGG, Inception, etc)
+    shared_layers = nn.nn_base(img_input, trainable=True)
+
+    # define the RPN, built on the base layers
+    num_anchors = len(C.anchor_box_scales) * len(C.anchor_box_ratios)
+    rpn_layers = nn.rpn(shared_layers, num_anchors)
+
+    classifier = nn.classifier(feature_map_input, roi_input, C.num_rois, nb_classes=len(class_mapping), trainable=True)
+
+    model_rpn = Model(img_input, rpn_layers)
+    model_classifier_only = Model([feature_map_input, roi_input], classifier)
+
+    model_classifier = Model([feature_map_input, roi_input], classifier)
+
+    model_rpn.load_weights(C.model_path, by_name=True)
+    model_classifier.load_weights(C.model_path, by_name=True)
+
+    model_rpn.compile(optimizer='sgd', loss='mse')
 
